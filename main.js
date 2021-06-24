47f4c26a77beb4e4ece62083e67e8eaf1ee37480
Milena <milenaalmeidadev@hotmail.com>
2021-06-24 08:58:28 -0400

    [Mission: Origin] Aula 04 – Aprofundando no JavaScript

--- main.js
+++ main.js
@@ -15,16 +15,16 @@ for(const link of links) {
     });
 }
 
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header');
+    const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll');
     } else {
         header.classList.remove('scroll');
     }
-});
+}
 
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
@@ -40,14 +40,30 @@ const scrollReveal = ScrollReveal({
     distance: '30px',
     duration: 700,
     reset: true
-  });
+});
   
-  scrollReveal.reveal(
+scrollReveal.reveal(
     `#home .image, #home .text,
     #about .image, #about .text,
     #services header, #services .card,
-    #testimonials header, #testimonials .testimonials
-    #contact .text, #contact .links
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
     `,
     { interval: 100 }
-  );
+);
+
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top');
+
+    if(window.scrollY >= 560) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+}
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll();
+    backToTop();
+});
